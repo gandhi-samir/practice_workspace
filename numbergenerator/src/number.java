9dce5c1d848a838458e07a39d9263b0c50ff0825
Samir Gandhi <77183760+gandhi-samir@users.noreply.github.com>
2021-08-01 16:32:46 -0400

    new random project

---
+++ numbergenerator/src/number.java
@@ -0,0 +1,16 @@
+
+public class number {
+
+    public static void main (String[] args) {
+int j =0;
+        for(int i =-3;i <100; i++){
+
+            System.out.println(i+j);
+            j++;
+            i=i+j;
+
+
+        }
+
+    }
+}
